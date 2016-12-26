049356402fb6ad1365fbed0d4f73ba3774267087
Ricky Lung <dard12@gmail.com>
2016-12-25 22:14:51 -0500

    try to store consolelogs as data

--- imports/app.jsx
+++ imports/app.jsx
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 
 import Log from './log.jsx';
 
-// App component - represents the whole app
 export default class App extends Component {
-  getLogs() {
-    return [
-      { _id: 1, text: 'You are in a dark tomb.' },
-      { _id: 2, text: 'The air is damp.' },
-      { _id: 3, text: 'A skeleton is here.' },
+  constructor(props) {
+    super(props);
+
+    this.consoleLogs = [
+      {_id: 1, text: 'You are in a dark tomb.'},
+      {_id: 2, text: 'The air is damp.'},
+      {_id: 3, text: 'A skeleton is here.'},
     ];
   }
 
   renderLogs() {
-    return this.getLogs().map((log) => (
+    return this.consoleLogs.map((log) => (
       <Log key={log._id} text={log.text} />
     ));
+  }
+
+  submitCommand() {
+    const newLog = {_id: Random.id(), text: 'You are in a dark tomb.'};
+    this.consoleLogs.push(newLog);
   }
 
   render() {
@@ -26,7 +32,10 @@ export default class App extends Component {
             {this.renderLogs()}
           </ul>
 
-          <input type="text" className="console-input"></input>
+          <input type="text"
+                 className="console-input"
+                 onSubmit={this.submitCommand}>
+          </input>
         </div>
       </div>
     );
--- imports/log.jsx
+++ imports/log.jsx
@@ -9,7 +9,5 @@ export default class Log extends Component {
 }
 
 Log.propTypes = {
-  // This component gets the task to display through a React prop.
-  // We can use propTypes to indicate it is required
   text: PropTypes.object.isRequired,
 };
